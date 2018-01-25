878dc29daabab1a9dcde6c63ac7c98df3d1bb521
Yuri Dirickson <ydirickson@gmail.com>
2018-01-25 12:49:15 -0200

    Mudando para docs

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\5894272\documents\ymrd-git\pannano-proceedings\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
