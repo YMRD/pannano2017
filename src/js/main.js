25bf9a57d4f4cb9743cc047e21c45a4a085f3fe9
Yuri Dirickson <ydirickson@gmail.com>
2018-07-22 21:48:22 -0300

    Scroll smooth

--- src/js/main.js
+++ src/js/main.js
@@ -15,14 +15,31 @@ var main = (function(){
         // Removing splash and showing main content
         $("#splash").remove();
         $("header, footer, main").addClass("loaded");
-        $("#navigator").click("a.nav-link", function(event) {
-            event.preventDefault();
-            var target = $(this).attr("href").substring(1),
-                zoom =  $("#"+target).offset().top;
+        $("#navigator a.nav-link").click(function(event) {
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                console.log("Scrollando para "+$(this).attr("href"));
+                // Prevent default anchor click behavior
+                event.preventDefault();
 
-            $('html,body').animate({
-                scrollTop: zoom},
-                'slow');
+                // Store hash
+                var hash = this.hash;
+
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                scrollTop: $(hash).offset().top - 300
+                }, 800, function(){
+            
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                if(history.pushState) {
+                    history.pushState(null, null, hash);
+                }
+                else {
+                    location.hash = hash;
+                }
+                });
+            }
         });
         // Listening to page scroll
         $(window).scroll(function() {
